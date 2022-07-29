f0d5eae68080fb089fe0c78311a4d3271a51819a
Binh Tran <tranngocbaobinh@gmail.com>
2022-07-29 13:19:31 +0700

    initial

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(__dirname + "/dist/air-bnb"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/air-bnb/index.html"));
+});
+
+// app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
